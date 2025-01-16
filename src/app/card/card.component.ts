8278f020113598d15d40fa3a3b0740d0e1ddd3f3
gizuka@nownow.ng <gizuka@nownow.ng>
2025-01-16 13:35:24 +0100

    added security

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,25 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabService } from '../services/tab.service';
+import { ToastService } from '../shared/services/toast.service';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import PaystackPop from '@paystack/inline-js'
+// import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-card',
-  imports: [CommonModule, HttpClientModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrls: ['./card.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule],
+  standalone: true
 })
-export class CardComponent {
+export class CardComponent implements OnInit, OnDestroy {
   activeTab$: any;
-  
-  constructor(private router: Router,
-    private tabService: TabService) {
+  private readonly SESSION_TIMEOUT = 600000; // 10 minutes
+  private sessionTimer: any;
+  cardForm!: FormGroup;
 
-      this.activeTab$ = this.tabService.activeTab$;
-     }
+  
+
+  constructor(
+    private router: Router,
+    private tabService: TabService,
+    private fb: FormBuilder,
+    private toastService: ToastService
+  ) {
+    this.activeTab$ = this.tabService.activeTab$;
+    this.initForm();
     
+  }
+
+
+  ngOnInit() {
+    this.startSessionTimer();
+    this.initializeFormDefaults();
+    
+  }
+
+  ngOnDestroy() {
+    this.clearSensitiveData();
+    if (this.sessionTimer) {
+      clearTimeout(this.sessionTimer);
+    }
+  }
+
+  
+
   navigateToCard() {
     this.tabService.setActiveTab('card');
     this.router.navigate(['/card']);
@@ -35,9 +63,70 @@ export class CardComponent {
     this.router.navigate(['/new-transfer']);
   }
 
-  initiatePayStack(){
-    // const access_code = { accessCode: '32323242324232424232423' }
-    // const popup = new PaystackPop();
-    // popup.resumeTransaction(access_code)
+  maskCardNumber(event: any) {
+    let input = event.target.value.replace(/\D/g, '');
+    if (input.length > 16) {
+      input = input.substr(0, 16);
+    }
+    const masked = input.replace(/(\d{4})/g, '$1 ').trim();
+    console.log(masked);
+    event.target.value = masked;
+    this.cardForm.patchValue({ cardNumber: input }, { emitEvent: false });
+  }
+
+  formatExpiryDate(event: any) {
+    let input = event.target.value.replace(/\D/g, '');
+    if (input.length >= 2) {
+      input = input.substr(0, 2) + '/' + input.substr(2);
+    }
+    event.target.value = input;
+  }
+
+  initiatePayStack() {
+    // if (this.cardForm.valid) {
+    //   const securePayload = this.prepareSecurePayload();
+    //   // Implement PayStack integration here
+    //   this.toastService.show(true);
+    // }
+    console.log(this.cardForm.value);
+  }
+
+  private startSessionTimer() {
+    this.sessionTimer = setTimeout(() => {
+      this.navigateToNewTransfer();
+      this.clearSensitiveData();
+    }, this.SESSION_TIMEOUT);
+  }
+
+  private clearSensitiveData() {
+    this.cardForm.reset();
+  }
+
+  private prepareSecurePayload() {
+    const formValue = this.cardForm.value;
+    return {
+      reference: formValue.paymentReference,
+      name: formValue.fullName,
+      cardLastFour: formValue.cardNumber.slice(-4),
+      expiryMonth: formValue.expirationDate.split('/')[0],
+      expiryYear: formValue.expirationDate.split('/')[1]
+    };
+  }
+
+  private initializeFormDefaults() {
+    this.cardForm.patchValue({
+      paymentReference: '45417280',
+      fullName: 'John Jane Doe'
+    });
+  }
+
+  private initForm(): void {
+    this.cardForm = this.fb.group({
+      paymentReference: ['', [Validators.required, Validators.pattern('^[0-9]{8,12}$')]],
+      fullName: ['', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]],
+      cardNumber: ['', [Validators.required, Validators.pattern('^[0-9]{16}$')]],
+      expirationDate: ['', [Validators.required, Validators.pattern('^(0[1-9]|1[0-2])\/([0-9]{2})$')]],
+      cvv: ['', [Validators.required, Validators.pattern('^[0-9]{3}$')]]
+    });
   }
 }
